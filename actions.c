83fb6c36003af2f291804c95b02cc325101b1bd2
Thiwanka Somachandra <thivankas@outlook.com>
2025-05-21 07:44:47 +0300

    Re-arrange the lock sequence and proper error handling

--- actions.c
+++ actions.c
@@ -6,7 +6,7 @@
 /*   By: tsomacha <tsomacha@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 04:33:21 by tsomacha          #+#    #+#             */
-/*   Updated: 2025/05/21 06:08:47 by tsomacha         ###   ########.fr       */
+/*   Updated: 2025/05/21 07:42:17 by tsomacha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@
 int		ft_think(t_philo *philo);
 int		ft_eat(t_philo *philo);
 int		ft_sleep(t_philo *philo);
-void	order(t_philo *philo, pthread_mutex_t **l1, pthread_mutex_t **l2);
 
 /**
  * philosopher's think routine function
@@ -38,15 +37,14 @@ int	ft_think(t_philo *philo)
 int	ft_eat(t_philo *philo)
 {
 	size_t			time;
-	pthread_mutex_t	*l1;
-	pthread_mutex_t	*l2;
 
 	if (philo->size > 1)
 	{
-		order(philo, &l1, &l2);
-		pthread_mutex_lock(l1);
+		if (pthread_mutex_lock(&philo->fork_l->fork) != 0)
+			return (1);
 		ft_print(philo, "has taken a fork");
-		pthread_mutex_lock(l2);
+		if (pthread_mutex_lock(&philo->fork_r->fork) != 0)
+			return (pthread_mutex_unlock(&philo->fork_l->fork), 1);
 		ft_print(philo, "has taken a fork");
 		pthread_mutex_lock(philo->lck_mel);
 		time = getcurrenttime();
@@ -55,8 +53,8 @@ int	ft_eat(t_philo *philo)
 		pthread_mutex_unlock(philo->lck_mel);
 		ft_print(philo, "is eating");
 		ft_usleep(philo->tm_eat);
-		pthread_mutex_unlock(l2);
-		pthread_mutex_unlock(l1);
+		pthread_mutex_unlock(&philo->fork_r->fork);
+		pthread_mutex_unlock(&philo->fork_l->fork);
 		return (1);
 	}
 	else
@@ -71,18 +69,4 @@ int	ft_sleep(t_philo *philo)
 	ft_print(philo, "is sleeping");
 	ft_usleep(philo->tm_slp);
 	return (1);
-}
-
-void	order(t_philo *philo, pthread_mutex_t **l1, pthread_mutex_t **l2)
-{
-	if (&philo->fork_l->fork < &philo->fork_r->fork)
-	{
-		*l1 = &philo->fork_l->fork;
-		*l2 = &philo->fork_r->fork;
-	}
-	else
-	{
-		*l1 = &philo->fork_r->fork;
-		*l2 = &philo->fork_l->fork;
-	}
 }
